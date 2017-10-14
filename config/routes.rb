bd558cd8a0c506d750130229c3e80f6c93a2975b
Raja Muthukathan <raja.muthukathan@maktoinc.com>
2017-10-14 15:03:44 +0530

    navbar logo

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   resources :blogs
   get 'carton', :to => 'static#carton'
   get 'company', :to => 'static#company'
+  get 'team', :to => 'static#team'
+  get 'contactUs', :to => 'static#contactus'
+  get 'data-analytics', :to => 'static#analytics'
+  get 'data-visualization', :to => 'static#datavisualization'
+  get 'data-modeling', :to => 'static#datamodeling'
   get 'visitors/index'
   root :to => 'visitors#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
