53d57316191f273c32d60314c62d80ec0970f03e
Raja Muthukathan <raja.muthukathan@maktoinc.com>
2017-10-24 14:12:03 +0530

    accordion changed

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
   get 'team', :to => 'static#team'
   get 'contactUs', :to => 'static#contactus'
   get 'data-analytics', :to => 'static#analytics'
-  get 'data-visualization', :to => 'static#datavisualization'
-  get 'data-modeling', :to => 'static#datamodeling'
   get 'visitors/index'
   root :to => 'visitors#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
