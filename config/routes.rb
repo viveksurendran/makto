cafc49080e3e91f39150774a3a50c96d2f50d7d2
Raja Muthukathan <raja.muthukathan@maktoinc.com>
2017-10-25 18:29:30 +0530

    devise admin added

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :employees
+  devise_for :admins
+  root :to => 'visitors#index'
   resources :blogs
+  devise_for :employees
   get 'carton', :to => 'static#carton'
   get 'company', :to => 'static#company'
   get 'team', :to => 'static#team'
   get 'contactUs', :to => 'static#contactus'
   get 'data-analytics', :to => 'static#analytics'
   get 'visitors/index'
-  root :to => 'visitors#index'
+    authenticate :admin do
+   devise_scope :admin do
+       get 'admin-dashboard', to: 'admins/registrations#home', :as => :admin_root
+   end
+end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
